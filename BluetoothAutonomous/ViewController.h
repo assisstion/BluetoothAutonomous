c146bebeb42d09f1210356da8e86a92ab6634685
Markus Feng <zzz1112222595@hotmail.com>
2016-04-05 10:21:15 -0400

    Added accelerometer function
    Code by Aditya

--- BluetoothAutonomous/ViewController.h
+++ BluetoothAutonomous/ViewController.h
@@ -9,12 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "BluetoothControlReceiver.h"
 #import "BluetoothControl.h"
+#import <CoreMotion/CoreMotion.h>
+
+double currentMaxAccelX;
+double currentMaxAccelY;
+double currentMaxAccelZ;
+double currentMaxRotX;
+double currentMaxRotY;
+double currentMaxRotZ;
 
 @interface ViewController : UIViewController <BluetoothControlReceiver>
 
 @property BluetoothControl * control;
 @property (weak, nonatomic) IBOutlet UILabel *bluetoothStatusLabel;
 @property (weak, nonatomic) IBOutlet UIButton *powerButton;
+@property (strong, nonatomic) CMMotionManager *motionManager;
 
 @end
 
--- BluetoothAutonomous/ViewController.m
+++ BluetoothAutonomous/ViewController.m
@@ -22,6 +22,62 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.control = [[BluetoothControl alloc] initWithReceiver:self];
+    
+    currentMaxAccelX = 0;
+    currentMaxAccelY = 0;
+    currentMaxAccelZ = 0;
+    
+    currentMaxRotX = 0;
+    currentMaxRotY = 0;
+    currentMaxRotZ = 0;
+    self.motionManager = [[CMMotionManager alloc] init];
+    self.motionManager.accelerometerUpdateInterval = 1.0;
+    self.motionManager.gyroUpdateInterval = 1.0;
+    
+    [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue]
+                                             withHandler:^(CMAccelerometerData  *accelerometerData, NSError *error) {
+                                                 [self outputAccelerationData:accelerometerData.acceleration];
+                                                 if(error){
+                                                     NSLog(@"%@", error);
+                                                 }
+                                             }];
+    
+    [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue]
+                                    withHandler:^(CMGyroData *gyroData, NSError *error) {[self outputRotationData:gyroData.rotationRate];}];
+}
+
+//Motion Manager Functions
+-(void)outputAccelerationData:(CMAcceleration)acceleration
+{
+    /*NSLog(@"x - acceleration %@",[NSString stringWithFormat:@" %.2fg",acceleration.x]);
+    if(fabs(acceleration.x) > fabs(currentMaxAccelX))
+    {
+        currentMaxAccelX = acceleration.x;
+    }
+    NSLog(@"y-acceleration %@",[NSString stringWithFormat:@" %.2fg",acceleration.y]);
+    if(fabs(acceleration.y) > fabs(currentMaxAccelY))
+    {
+        currentMaxAccelY = acceleration.y;
+    }
+    NSLog(@"z-acceleration%@",[NSString stringWithFormat:@" %.2fg",acceleration.z]);
+    if(fabs(acceleration.z) > fabs(currentMaxAccelZ))
+    {
+        currentMaxAccelZ = acceleration.z;
+    }
+    
+    NSLog(@"maxx-accel%@",[NSString stringWithFormat:@" %.2f",currentMaxAccelX]);
+    NSLog(@"maxy-accel%@",[NSString stringWithFormat:@" %.2f",currentMaxAccelY]);
+    NSLog(@"maxz-accel%@",[NSString stringWithFormat:@" %.2f",currentMaxAccelZ]);*/
+    
+    
+}
+-(void)outputRotationData:(CMRotationRate)rotation
+{
+    /*NSLog(@"x-rotation%@",[NSString stringWithFormat:@" %.2fr/s",rotation.x]);
+    NSLog(@"y-rotation%@",[NSString stringWithFormat:@" %.2fr/s",rotation.y]);
+    NSLog(@"z-rotation%@",[NSString stringWithFormat:@" %.2fr/s",rotation.z]);*/
+    [self updateWithXRotation:rotation.x];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,26 +96,37 @@
 - (IBAction)startAction:(id)sender {
     if(!self.started){
         NSLog(@"Begin");
-        [self.control sendSpeedDataWithLeft:1 andRight:-1];
+        //[self.control sendSpeedDataWithLeft:1 andRight:-1];
         [self.powerButton setTitle:@"Stop" forState:UIControlStateNormal];
         self.started = true;
     }
     else{
         NSLog(@"End");
-        [self.control sendSpeedDataWithLeft:0 andRight:0];
+        //[self.control sendSpeedDataWithLeft:0 andRight:0];
         [self.powerButton setTitle:@"Start" forState:UIControlStateNormal];
         self.started = false;
     }
 }
 
 -(void)updateWithXRotation:(double)rotation{
-    if(rotation > 0){
-        //move backwards
-        [self.control sendSpeedDataWithLeft:-1 andRight:-1];
+    if(self.started){
+        double threshold = 0.5;
+        if(rotation > threshold){
+            //move forwards
+            [self.control sendSpeedDataWithLeft:1 andRight:1];
+        }
+        else if(rotation < -threshold){
+            //move backwards
+            [self.control sendSpeedDataWithLeft:-1 andRight:-1];
+        }
+        else{
+            //stop
+            [self.control sendSpeedDataWithLeft:0 andRight:0];
+        }
     }
     else{
-        //move forwards
-        [self.control sendSpeedDataWithLeft:1 andRight:1];
+        //stop
+        [self.control sendSpeedDataWithLeft:0 andRight:0];
     }
 }
 
